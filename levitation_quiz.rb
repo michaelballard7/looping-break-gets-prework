c7bf51f3b6eee34d10e029890b154c6c571686a8
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 01:10:56 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
-	until 
-end
+	keyword = false
+	while keyword != true 
+	puts "What is the spell that enacts levitation?"
+	spell = gets.chomp
+	if spell == "Wingardium Leviosa"
+	  break
+
 
 
