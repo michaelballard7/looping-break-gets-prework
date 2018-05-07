1d4b875160bf947e85454ef6bbbb4b96dca25d5f
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 01:14:16 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
-	#your code here
+	keyword = false
+    while keyword != true 
+    	puts "What is the spell that enacts levitation?"
+    	spell = gets.chomp
+    	if spell == "Wingardium Leviosa"
+    	  puts "You passed the quiz!"
+    	  keyword = true
+      end
+	  end
 end
-
 
